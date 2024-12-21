bef31e9dcfc295e727e4a06c57ed45f6f13a481a
Galeed Gutierrez <galeedeveloper@gmail.com>
2024-12-21 15:16:58 -0300

    feat: :sparkles: Definir el prototipo de DevDash_ con Figma y la estrategia de desarrollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { JSX, useEffect } from 'react';
 
 import styles from './App.module.css';
-import SkipLinks from './components/SkipLink';
-import UserCard from './components/UserCard';
-import { useUsers } from './components/UserCard/useUsers';
-import handleClick from './utils/applyTheme';
+import { Dashboard } from './components/Dashboard';
 
-function App(): JSX.Element {
+export function App(): JSX.Element {
 	useEffect(() => {
 		function updateTheme({ matches }: MediaQueryListEvent): void {
 			document.body.classList.remove(matches ? 'light' : 'dark');
@@ -23,31 +20,8 @@ function App(): JSX.Element {
 	}, []);
 
 	return (
-		<>
-			<nav>
-				<SkipLinks />
-			</nav>
-			<main className={`${styles['g-app']} max-width-desktop`}>
-				<h1 className="text-center">
-					⚡ Vite React Best Practices Template (by Codely) ⚛️
-				</h1>
-				<h2>Current users</h2>
-				{useUsers().map((user) => (
-					<UserCard
-						key={user.name}
-						user={user}
-					/>
-				))}
-				<button
-					className="a-button a-button-primary"
-					type="button"
-					onClick={handleClick}
-				>
-					Change Theme
-				</button>
-			</main>
-		</>
+		<main className={`${styles['g-app']} max-width-desktop`}>
+			<Dashboard />
+		</main>
 	);
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import './styles/index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App.tsx';
+import { App } from './App.tsx';
 import ErrorBoundary from './ErrorBoundary.tsx';
 
 const rootElement = document.querySelector('#root');
