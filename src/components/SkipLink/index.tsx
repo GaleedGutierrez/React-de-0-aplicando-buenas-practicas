a0a73558681c579f432803c5b94d1dc01c177db7
Galeed Gutierrez <galeedeveloper@gmail.com>
2024-12-21 01:23:39 -0300

    first commit

---
+++ src/components/SkipLink/index.tsx
@@ -0,0 +1,19 @@
+import { JSX } from 'react';
+
+function SkipLinks(): JSX.Element {
+	return (
+		<ul>
+			<li>
+				<a
+					aria-label='Skip to "Vite Documentation"'
+					className="is-skip-link"
+					href="https://vite.dev/guide/"
+				>
+					Skip Link
+				</a>
+			</li>
+		</ul>
+	);
+}
+
+export default SkipLinks;
