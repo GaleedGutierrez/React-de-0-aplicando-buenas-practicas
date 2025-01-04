97227a2804f790419051435667eb453146454dc5
Galeed Gutierrez <galeedeveloper@gmail.com>
2025-01-04 00:17:29 -0300

    feat: :sparkles: Implement ErrorBoundary component for error handling in the application
    Control de errores de componentes: Gestión de excepciones con ErrorBoundaries

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
-import ErrorBoundary from './ErrorBoundary';
+import { ErrorBoundary } from './ErrorBoundary';
 
 const rootElement = document.querySelector('#root');
 
